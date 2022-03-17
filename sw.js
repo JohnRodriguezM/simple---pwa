464a0ff6bca4ff2ff0695cd3c0877ef6a12c7440
john jairo rodriguez medina <johnjairorodriguez384@gmail.com>
2022-03-16 20:56:51 -0500

    firebase

--- sw.js
+++ sw.js
@@ -3,13 +3,25 @@
 let CACHE_NAME = "v1_cache_aprende_web";
 
 let urlToCache = [
+  // se guarda la raiz del documento
   "./",
+  // se guarda el documento index.html
+  "./index.html",
+  // se guarda el cdnjs de la libreria hamburger.css
   "https://cdnjs.cloudflare.com/ajax/libs/hamburgers/1.1.3/hamburgers.min.css",
+  // se guarda el cnjs de la libreria bulma.css
   "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.3/css/bulma.min.css",
+  // se guarda el cnjs de la libreria font-awesome
   "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css",
-  "./styles.css",
+  // se guarda el archivo de estilo
+  "./css/styles.css",
+  // archivo en donde se condiciona la existencia del serviceWorker
   "./index.js",
+  // archivo en donde se manda a llamar firebase
+  "./js/firebase/appFirebase.mjs",
+  // archivo donde se hace interactiva la navbar
   "./js/navBar.js",
+  // imagenes del archivo
   "./assets/logo.webp",
   "./assets/images.png",
 ];
@@ -29,7 +41,7 @@ self.addEventListener("install", (e) => {
   );
 });
 
- /* le vamos a programar eventos que incluso se ejecuten sin conexion */
+/* le vamos a programar eventos que incluso se ejecuten sin conexion */
 self.addEventListener("activate", (e) => {
   const cacheWhiteList = [CACHE_NAME];
 
@@ -63,12 +75,10 @@ self.addEventListener("fetch", (e) => {
       // solicta nuevamente a la url
       return fetch(e.request);
     })
-    );
-  });
-  
+  );
+});
 
-  
-  /*
+/*
   1. asignar un nombre a la version de nuestro cache y una de las caracteristicas que tienen las pwa es que nos permiten guardar en cache todos los recurso estáticos que a lo mejor no van a cambiar en nuestra app y que los podemos guardar directamente en el disco duro del usuario
   
   adicionalmente la pwa tiene una caracteristicas un poco mas avanzadas
@@ -76,12 +86,12 @@ self.addEventListener("fetch", (e) => {
   - API DE SINCRONIZACION EN SEGUNDO PLANO
   - LANZAR NOTIFICACIONES PUS
   */
- 
- // entonces creo mi nombre de cache
- 
- // ahora, los serviceWorker van a tener 3 eventos muy importantes
- // el self es para hacer referencia a si mismo
- /*
+
+// entonces creo mi nombre de cache
+
+// ahora, los serviceWorker van a tener 3 eventos muy importantes
+// el self es para hacer referencia a si mismo
+/*
  1.
  durante la fase de instalacion, generalmente se alamacena en cache los archivos estáticos
  */
