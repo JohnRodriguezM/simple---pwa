221ad833876fb9499b3f9008986326e8393deedc
john jairo rodriguez medina <johnjairorodriguez384@gmail.com>
2022-03-17 22:46:14 -0500

    sw active

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
 //se condiciona la existencia o la capacidad del uso del service worker dentro del navegador
 // entonces se hace el condicional
 // luego de eso se hace el 'registro del service worker que en este caso va a estar en el archivo sw.js'
-/* if('serviceWorker' in navigator){
+if('serviceWorker' in navigator){
     navigator.serviceWorker.register('./sw.js')
     .then(res => console.log(`registro ${res}`))
     .catch(err => console.error(`haz tenido un error: ${err}`))
 }
- */
+
 
 
 
